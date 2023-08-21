a85cb655ca5a18c17505b2c62c0ec365b9ffaf1f
Ayrton Rafael Ribeiro de Souza <mrton.developer@gmail.com>
2023-08-21 20:11:43 -0300

    Dex React 0.0

---
+++ dex/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import './getInfoData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <div className='bg'>
+            <div className='bg-dex'>
+              <div className='sMenu'>
+                  <h2> Nome ou número</h2>
+                  <div className='dBar'></div>
+                     
+                    <div className='app-dex' >
+                    <h2> Pokédex</h2>
+                
+                
+                    </div>
+              </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
