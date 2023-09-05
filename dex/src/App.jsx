f45646bffe2a2faf262fef50284dfd08abbcc58e
Ayrton Rafael Ribeiro de Souza <mrton.developer@gmail.com>
2023-09-05 15:44:06 -0300

    Definição de tipos e tradução

--- dex/src/App.jsx
+++ dex/src/App.jsx
@@ -1,11 +1,36 @@
 import React, { useState } from "react"
-import './App.css'; 
+import './App.css';
+import './index.css';
+
+// Mapeamento de tipos em inglês para português
+const typeTranslations = {
+  normal: 'Normal',
+  fighting: 'Lutador',
+  flying: 'Voador',
+  fire: 'Fogo',
+  bug: 'Inseto',
+  eletric: 'Elétrico',
+  ice: 'Gelo',
+  dark: 'Sombrio',
+  rock: 'Pedra',
+  ground: 'Terrestre',
+  water: 'Aquático',
+  poison: 'Venenoso',
+  ghost: 'Fantasma',
+  dragon: 'Dragão',
+  stell: 'Aço',
+  grass: 'Planta',
+  fairy: 'Fada',
+
+  // Adicione os outros tipos conforme necessário
+};
 
 const App = () => {
+  const [card, setCard] = useState(null);
   const [pName, setpName] = useState('');
   const [namedata, setnameData] = useState(null);
   const [iddata, setiddata] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null); // Estado para armazenar a URL da imagem
+  const [imageUrl, setImageUrl] = useState(null);
 
   const change = (event) => {
     setpName(event.target.value);
@@ -21,12 +46,13 @@ const App = () => {
       .then((response) => response.json())
       .then((data) => {
         // Manipular dados da API  
-        // console.log(data);
-
+        console.log(data);
+        setCard(data);
         setnameData(data.name);
         setiddata(data.id);
         // Definir a URL da imagem
-        setImageUrl(data['sprites']['other']['official-artwork']['front_default']); 
+        setImageUrl(data['sprites']['other']['official-artwork']['front_default']);
+
       })
       .catch((error) => {
         // Manipular erros
@@ -41,18 +67,25 @@ const App = () => {
           <div className='title'><h2> Pokédex </h2></div>
           <div className='searchBar'>
             <form onSubmit={baseurl}>
-            <h2>Nome ou número</h2>
-            <input type='text' value={pName} onChange={change}></input>
-            <button onClick={baseurl}> <img className='lupa' alt='pesquisa' src={require('./lupa.svg').default} /></button>
+              <h2>Nome ou número</h2>
+              <input type='text' value={pName} onChange={change}></input>
+              <button onClick={baseurl}> <img className='lupa' alt='pesquisa' src={require('./lupa.svg').default} /></button>
             </form>
           </div>
           <div className='grayBar'></div>
           <div className='pokedexBG'>
-            <div className="CardPoke">
-            {imageUrl && <img className="sPokeImg" src={imageUrl} alt="Imagem do Pokémon" />}
-            {namedata &&  <div className="sPokeName"> <p> Nome: {namedata} </p></div> }
-            {iddata &&  <div className="sPokeNumber" > <p> ID: {iddata}</p></div> }
-            </div>
+            {card && <div className="CardPoke">
+              <img className="sPokeImg" src={imageUrl} alt="Imagem do Pokémon" />
+              <div className="sPokeNumber" > <p> N° {iddata}</p></div>
+              <div className="sPokeName" >
+                <p>{namedata && namedata.charAt(0).toUpperCase() + namedata.slice(1)}</p>
+              </div>
+              <div className="sPokeTypes">
+                {card.types.map((typeData) => (
+                  <span key={typeData.type.name} className="type">{typeTranslations[typeData.type.name]}</span>
+                ))}
+              </div>
+            </div>}
           </div>
         </div>
       </main>
