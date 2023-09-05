f38e2b26865a9e304a4b0004e050ba092ee4e68a
Ayrton Rafael Ribeiro de Souza <mrton.developer@gmail.com>
2023-09-05 04:55:31 -0300

    Dex Core

--- dex/src/App.jsx
+++ dex/src/App.jsx
@@ -5,13 +5,17 @@ const App = () => {
   const [pName, setpName] = useState('');
   const [namedata, setnameData] = useState('');
   const [iddata, setiddata] = useState('');
+  const [imageUrl, setImageUrl] = useState(null); // Estado para armazenar a URL da imagem
 
   const change = (event) => {
     setpName(event.target.value);
   }
 
-  const baseurl = () => {
-    const Url = `https://pokeapi.co/api/v2/pokemon/${pName}`;
+  const baseurl = (event) => {
+    event.preventDefault()
+
+    const lowercaseName = pName.toLowerCase();
+    const Url = `https://pokeapi.co/api/v2/pokemon/${lowercaseName}`;
 
     fetch(Url)
       .then((response) => response.json())
@@ -21,6 +25,7 @@ const App = () => {
 
         setnameData(data.name);
         setiddata(data.id);
+        setImageUrl(data['sprites']['other']['official-artwork']['front_default']); // Definir a URL da imagem
       })
       .catch((error) => {
         // Manipular erros
@@ -34,13 +39,16 @@ const App = () => {
         <div className='container'>
           <div className='title'><h2> Pokédex </h2></div>
           <div className='searchBar'>
+            <form onSubmit={baseurl}>
             <h2>Nome ou número</h2>
             <input type='text' value={pName} onChange={change}></input>
             <button onClick={baseurl}> <img className='lupa' alt='pesquisa' src={require('./lupa.svg').default} /></button>
+            </form>
           </div>
           <div className='grayBar'></div>
           <div className='pokedexBG'>
             <div className="CardPoke">
+            {imageUrl && <img className="sPokeImg" src={imageUrl} alt="Imagem do Pokémon" />}
               <div className="sPokeName"> <p> Nome: {namedata} </p></div>
               <div className="sPokeNumber" > <p> ID: {iddata}</p></div>
             </div>
